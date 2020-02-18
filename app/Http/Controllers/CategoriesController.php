c00be6b4dd6faafe095ed5f93924ff9067b43be0
SOURAV ROY <souravroy.info13@gmail.com>
2020-02-18 13:24:42 +0600

    Delete Categories Work Fine With JS ⚙

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -108,8 +108,12 @@ class CategoriesController extends Controller
      * @param int $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        session()->flash('success', 'Category Deleted Successfully !!!');
+
+        return redirect(route('categories.index'));
     }
 }
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -11,23 +11,28 @@
 
         <div class="card-body">
 
-            <table class="table table-bordered">
+            <table class="table table-bordered text-center">
 
                 <thead class="thead-light">
                 <tr>
                     <th scope="col">NAME</th>
-                    <th></th>
+                    <th colspan="2">ACTIONS</th>
                 </tr>
                 </thead>
 
-                <tbody>
+                <tbody class="text-center">
                 @foreach($categories as $category)
                     <tr>
                         <td>
                             {{$category->name}}
                         </td>
                         <td>
-                            <a href="{{route('categories.edit',$category->id)}}" class="btn btn-info">EDIT</a>
+                            <a href="{{route('categories.edit',$category->id)}}"
+                               class="btn btn-info btn-sm">EDIT</a>
+                        </td>
+                        <td>
+                            <button class="btn btn-danger btn-sm" onclick="handleDelete({{$category->id}})">DELETE
+                            </button>
                         </td>
                     </tr>
                 @endforeach
@@ -35,7 +40,55 @@
 
             </table>
 
+
+            <!-- Modal -->
+            <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel"
+                 aria-hidden="true">
+                <div class="modal-dialog" role="document">
+
+                    <form action="" method="POST" id="deleteCategoryForm">
+                        @csrf
+                        @method('DELETE')
+
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="deleteModalLabel">Delete Category</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <p class="text-center font-weight-bold">
+                                    Are You Sure Delete Category !!
+                                </p>
+                                {{--                                <h2>{{$category->name}}</h2>--}}
+
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">NO GoBack</button>
+                                <button type="submit" class="btn btn-danger">YES Delete</button>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+
         </div>
     </div>
+
+@endsection
+
+@section('scripts')
+    <script>
+        function handleDelete(id) {
+            var form = document.getElementById('deleteCategoryForm');
+            form.action = '/categories/' + id;
+
+            console.log('delete call', form);
+
+            $('#deleteModal').modal('show')
+        }
+    </script>
 
 @endsection
