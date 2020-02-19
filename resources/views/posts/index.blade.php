ac37fb59708d74768d05623ff13dd7430de46a1a
SOURAV ROY <souravroy.info13@gmail.com>
2020-02-19 12:19:11 +0600

    Image Link Work AWESOME 👏✔

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,5 +7,33 @@
 
     <div class="card card-default">
         <h4 class="card-header text-center font-weight-bold">Posts</h4>
+
+        <div class="card card-body">
+            <table class="table table-bordered text-center">
+
+                <thead class="thead-light">
+                <tr>
+                    <th>IMAGE</th>
+                    <th>TITLE</th>
+                </tr>
+                </thead>
+
+                <tbody class="text-center">
+                @foreach($posts as $post)
+                    <tr>
+                        <td>
+                            <img src="/storage/{{ $post->image }}" width="120px" height="60px" alt="Image">
+                        </td>
+
+                        <td>
+                            {{$post->title}}
+                        </td>
+
+                    </tr>
+                @endforeach
+                </tbody>
+
+            </table>
+        </div>
     </div>
 @endsection
