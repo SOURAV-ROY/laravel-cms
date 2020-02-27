c6a7ed99c9022e0eebfaa1eab969d02a3dab36d0
SOURAV ROY <souravroy.info13@gmail.com>
2020-02-27 12:47:52 +0600

    Add Link In Tags From Single Blog ✔

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -50,7 +50,7 @@
 
                         <div class="gap-xy-2 mt-6">
                             @foreach($post->tags as $tag)
-                                <a class="badge badge-pill badge-secondary" href="#">
+                                <a class="badge badge-pill badge-secondary" href="{{route('blog.tag', $tag->id)}}">
                                     {{$tag->name}}
                                 </a>
                             @endforeach
