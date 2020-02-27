fc500e59dfe8ff779c488c3719d6d27e53c78af9
SOURAV ROY <souravroy.info13@gmail.com>
2020-02-27 12:27:09 +0600

    scopeSearched(){} Used Successfully DONE 👍⚙

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,21 +11,22 @@ class WelcomeController extends Controller
 {
     public function index()
     {
-        $search = request()->query('search');
-
-        if ($search) {
-//            dd(request()->query('search'));
-            $posts = Post::where('title', 'LIKE', "%{$search}%")->simplePaginate(2);
-        } else {
-            $posts = Post::simplePaginate(4);
-        }
+//        $search = request()->query('search');
+//
+//        if ($search) {
+////            dd(request()->query('search'));
+//            $posts = Post::where('title', 'LIKE', "%{$search}%")->simplePaginate(2);
+//        } else {
+//            $posts = Post::simplePaginate(4);
+//        }
 
         return view('welcome')
             ->with('categories', Category::all())
             ->with('tags', Tag::all())
-            ->with('posts', $posts);
+            ->with('posts', Post::searched()->simplePaginate(4));
+//          ->with('posts', $posts);
 
-//            ->with('posts', Post::simplePaginate(2));
+//          ->with('posts', Post::simplePaginate(2));
 //          ->with('posts', Post::all());
     }
 }
